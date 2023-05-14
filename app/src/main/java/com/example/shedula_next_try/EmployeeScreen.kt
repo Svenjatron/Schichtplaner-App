1847bc3b9e5b20057fa98c43a9657c27ba085107
Svenjatron <95627941+Svenjatron@users.noreply.github.com>
2023-05-14 21:20:28 +0200

    Neue Screens

--- app/src/main/java/com/example/shedula_next_try/EmployeeScreen.kt
+++ app/src/main/java/com/example/shedula_next_try/EmployeeScreen.kt
@@ -3,18 +3,11 @@ package com.example.shedula_next_try
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -22,21 +15,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.*
-
-import com.example.shedula_next_try.User
-
-
 @Composable
-fun LoginManager() {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    val usernameState = remember { mutableStateOf("") }
-    val passwordState = remember { mutableStateOf("") }
+fun EmployeeScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -49,7 +32,7 @@ fun LoginManager() {
                 .padding(start = 20.dp, top = 20.dp, end = 20.dp)
                 .background(
                     brush = Brush.horizontalGradient(
-                        colors = listOf(unserOcker, com.example.shedula_next_try.ui.theme.orange2),
+                        colors = listOf(com.example.shedula_next_try.ui.theme.orange2, com.example.shedula_next_try.ui.theme.unserOcker),
                         startX = 0f,
                         endX = 500f
                     )
@@ -63,7 +46,7 @@ fun LoginManager() {
                 .padding(start = 20.dp, bottom = 20.dp, end = 20.dp)
                 .background(
                     brush = Brush.horizontalGradient(
-                        colors = listOf(orange2, unserOcker),
+                        colors = listOf(com.example.shedula_next_try.ui.theme.unserOcker, com.example.shedula_next_try.ui.theme.orange2),
                         startX = 0f,
                         endX = 500f
                     )
@@ -71,64 +54,41 @@ fun LoginManager() {
                 .align(Alignment.BottomCenter)
         )
 
-
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-
-            Text(
-                text = "Username",
-                modifier = Modifier.padding(start = 20.dp, top = 20.dp),
-                fontSize = 20.sp
-            )
-
-            BasicTextField(
-                value = username,
-                onValueChange = { username = it },
-                textStyle = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .background(leichtesGrau, shape = RoundedCornerShape(8.dp))
-                    .padding(horizontal = 20.dp, vertical = 20.dp)
-            )
-
-            Text(
-                text = "Passwort",
-                modifier = Modifier.padding(start = 20.dp, top = 20.dp),
-                fontSize = 20.sp
-            )
-
-            BasicTextField(
-                value = password,
-                onValueChange = { password = it },
-                textStyle = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .background(leichtesGrau, shape = RoundedCornerShape(8.dp))
-                    .padding(horizontal = 20.dp, vertical = 20.dp)
-            )
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-
             Button(
-                onClick = { /* Nach Eingabe der Login-Daten */ },
+                onClick = { /* Zur Login-Seite */ },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 20.dp, start = 20.dp, end = 20.dp)
                     .height(130.dp),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = leichtesGrau,
-                    contentColor = unserSchwarz
+                    containerColor = com.example.shedula_next_try.ui.theme.leichtesGrau,
+                    contentColor = com.example.shedula_next_try.ui.theme.unserSchwarz
                 )
             ) {
                 Text(
-                    text = "Anmelden",
-                    fontSize = 45.sp
+                    text = "Arbeitszeitmanagement",
+                    fontSize = 28.sp
+                )
+            }
+            Button(
+                onClick = { /* Zur Ersteinrichtung */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, start = 20.dp, end = 20.dp)
+                    .height(130.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = com.example.shedula_next_try.ui.theme.leichtesGrau,
+                    contentColor = com.example.shedula_next_try.ui.theme.unserSchwarz
+                )
+            ) {
+                Text(
+                    text = "Zeiterfassung aufrufen",
+                    fontSize = 28.sp
                 )
             }
         }
