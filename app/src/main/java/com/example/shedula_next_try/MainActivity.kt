0ff0ea71e715de118ba7c6698e523e0a434ea2e7
Svenjatron <95627941+Svenjatron@users.noreply.github.com>
2023-05-15 10:45:44 +0200

    Neue Screens

--- app/src/main/java/com/example/shedula_next_try/MainActivity.kt
+++ app/src/main/java/com/example/shedula_next_try/MainActivity.kt
@@ -12,14 +12,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginManager()
+            EmployeeScreen()
         }
     }
 }
 @Preview
 @Composable
-fun PreviewLoginManager() {
-    LoginManager()
+fun PreviewEmployeeScreen() {
+    EmployeeScreen()
 }
 
 
