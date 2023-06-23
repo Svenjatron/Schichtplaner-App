0fbf5b583772ac9d36f17613181f57930c3298f9
prerit123456 <prerit.singh@smail.th-koeln.de>
2023-06-23 21:39:51 +0200

    CHECK 4

--- app/src/main/java/com/example/shedula_next_try/Activity/MainActivity.kt
+++ app/src/main/java/com/example/shedula_next_try/Activity/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.shedula_next_try.Activity
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +17,7 @@ import com.example.shedula_next_try.View.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.FirebaseApp
 import androidx.lifecycle.ViewModelProvider
 import com.example.shedula_next_try.AdminRegister
@@ -25,17 +30,71 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var viewModel: MainViewModel
     private var admin: User? = null
-
+    private var nfcAdapter: NfcAdapter? = null
+    private var punchInTime = MutableLiveData<Long>(0)
+    private var punchOutTime = MutableLiveData<Long>(0)
+    private var hoursWorked = MutableLiveData<String>("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         setContent {
             ShedulaNextTryApp(viewModel)
         }
     }
+    override fun onResume() {
+        super.onResume()
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        val intentFilter = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, arrayOf(intentFilter), null)
+    }
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleNfcIntent(intent)
+    }
+    private fun handleNfcIntent(intent: Intent?) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action || NfcAdapter.ACTION_TAG_DISCOVERED == intent?.action) {
+            if (punchInTime.value == 0L) {
+                punchInTime.value = System.currentTimeMillis()
+                // ... Communicate punch in
+            } else if (punchOutTime.value == 0L) {
+                punchOutTime.value = System.currentTimeMillis()
+                // ... Communicate punch out
+                calculateTimeDifference()
+            } else {
+                // Reset if both times are already recorded
+                punchInTime.value = 0
+                punchOutTime.value = 0
+                hoursWorked.value = ""
+                // ... Communicate reset
+            }
+        }
+    }
+
+    private fun calculateTimeDifference() {
+        val diff = punchOutTime.value!! - punchInTime.value!!
+
+        var seconds = diff / 1000
+        var minutes = seconds / 60
+        val hours = minutes / 60
+
+        minutes %= 60
+        seconds %= 60
+
+        hoursWorked.value = "$hours:$minutes"
+    }
+
 }
 
 
